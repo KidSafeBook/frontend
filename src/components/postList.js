df546af56c1d4771265f4d6989a73da435c86883
ChrisJohDev <chris@chrisjohannesson.com>
2024-04-19 21:22:28 +0200

    refactored PostList. Lists posts correctly now

--- src/components/postList.js
+++ src/components/postList.js
@@ -1,9 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-function PostList() {
+/**
+ * PostList component.
+ * 
+ * @returns {*} - JSX component.
+ */
+const PostList = () => {
   const [records, setRecords] = useState([]);
 
   useEffect(() => {
+    /**
+     * Fetch data from the backend.
+     */
     const fetchData = async () => {
       try {
         const response = await fetch("http://localhost:3010");
@@ -11,7 +19,7 @@ function PostList() {
         console.log(`PostList - data: ${JSON.stringify(json)}`);
         setRecords(json.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -22,15 +30,17 @@ function PostList() {
     <section className="flex flex-col">
       <h2>Post List</h2>
       {records.map((record, index) => (
-        <div key={index}>
-          <h3>{record.postId} - {record.authorId}</h3>
-          <p>{record.content}</p>
+        <div key={index} className="mt-4">
+          <input type="hidden" value={record.postId} />
+          <input type="hidden" value={record.authorId} />
+          <h4>{record.title}</h4>
+          <p className="p-2 " style={{ border: "3px solid grey", borderStyle: "inset" }}>{record.content}</p>
           <div>Likes: {record.likes}, Dislikes: {record.dislikes}</div>
           <hr />
         </div>
       ))}
     </section>
   );
-}
+};
 
 export default PostList;
