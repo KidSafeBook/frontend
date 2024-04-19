73fd486c377c67aba055c9417f52c329f4496070
ChrisJohDev <chris@chrisjohannesson.com>
2024-04-19 19:10:38 +0200

    Refactore Postlist

--- src/components/postList.js
+++ src/components/postList.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-/**
- * PostList component.
- * 
- * @returns {*} - JSX component.
- */
-const PostList = () => {
+function PostList() {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3010");
+        const json = await response.json();
+        console.log(`PostList - data: ${JSON.stringify(json)}`);
+        setRecords(json.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section className="flex flex-col">
       <h2>Post List</h2>
-      <div>Post 1</div>
-      <div>Post 2</div>
-      <div>Post 3</div>
-      <div>Post 4</div>
-      <div>Post 5</div>
-      <div>Post 6</div>
-      <div>Post 7</div>
-      <div>Post 8</div>
-      <div>Post 9</div>
-      <div>Post 10</div>
-      <div>Post 11</div>
+      {records.map((record, index) => (
+        <div key={index}>
+          <h3>{record.postId} - {record.authorId}</h3>
+          <p>{record.content}</p>
+          <div>Likes: {record.likes}, Dislikes: {record.dislikes}</div>
+          <hr />
+        </div>
+      ))}
     </section>
   );
-};
+}
 
 export default PostList;
